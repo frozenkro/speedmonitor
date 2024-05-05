21d08b224b3860a04b7caa51f3acff078f51c987
kaleb <kalebroyolson+github@gmail.com>
2024-05-05 00:19:01 -0500

    Add db and scheduling

--- speedmonitor/main.go
+++ speedmonitor/main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
-	//"context"
+	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/showwin/speedtest-go/speedtest"
-	//"github.com/influxdata/influxdb-client-go/v2"
+	"github.com/joho/godotenv"
+	"github.com/go-co-op/gocron"
+	"github.com/influxdata/influxdb-client-go/v2"
 )
 
 type SpeedData struct {
 	Latency time.Duration
 	DLSpeed speedtest.ByteRate
 	ULSpeed speedtest.ByteRate
+	Time time.Time
+}
+
+var dbUrl, dbToken, dbOrg, dbBucket string
+
+func init() {
+	checkError(godotenv.Load())
 }
 
 func main() {
-	data := runSpeedTest()
-	fmt.Printf("Latency: %s, Download: %s, Upload: %s\n", data.Latency, data.DLSpeed, data.ULSpeed)
+
+	dbUrl = getEnvVar("SPEEDMONITOR_DB_LOCATION")
+	dbToken = getEnvVar("SPEEDMONITOR_DB_TOKEN")
+	dbOrg = getEnvVar("SPEEDMONITOR_DB_ORG")
+	dbBucket = getEnvVar("SPEEDMONITOR_DB_BUCKET")
+
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(1).Minute().Do(func() {
+		data := runSpeedTest()
+		fmt.Printf("Latency: %s, Download: %s, Upload: %s\n", data.Latency, data.DLSpeed, data.ULSpeed)
+
+		saveTest(data)
+	})
 }
 
 func runSpeedTest() SpeedData {
@@ -32,10 +53,48 @@ func runSpeedTest() SpeedData {
 		checkError(s.DownloadTest())
 		checkError(s.UploadTest())
 
-		data = SpeedData{s.Latency, s.DLSpeed, s.ULSpeed}
+		data = SpeedData{s.Latency, s.DLSpeed, s.ULSpeed, time.Now()}
 		s.Context.Reset()
 	}
 	return data
+}
+
+func saveTest(data SpeedData) {
+
+	client := influxdb2.NewClient(dbUrl, dbToken)
+	writeAPI := client.WriteAPIBlocking(dbOrg, dbBucket)
+
+	sp := influxdb2.NewPointWithMeasurement("speed").
+		AddTag("unit", "byte").
+		AddField("download", data.DLSpeed).
+		AddField("upload", data.ULSpeed).
+		SetTime(data.Time)
+
+	lp := influxdb2.NewPointWithMeasurement("latency").
+		AddTag("unit", "ms").
+		AddField("delay", data.Latency).
+		SetTime(data.Time)
+		
+	sp_err := writeAPI.WritePoint(context.Background(), sp)
+
+	lp_err := writeAPI.WritePoint(context.Background(), lp)
+
+	if sp_err != nil {
+		log.Print(sp_err)
+	}
+	if lp_err != nil {
+		log.Print(lp_err)
+	}
+	
+	client.Close()
+}
+
+func getEnvVar(varKey string) string {
+	val, exists := os.LookupEnv(varKey)
+	if !exists {
+		log.Fatal("Env var '%s' missing", varKey)
+	}
+	return val
 }
 
 func checkError(err error) {
